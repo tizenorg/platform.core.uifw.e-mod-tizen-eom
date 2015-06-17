9b5b8b45f5cc3772debe0b692f40b27958a51035
SooChan Lim <sc1.lim@samsung.com>
2015-06-17 10:51:10 +0900

    add initial code
    Change-Id: Ic4d1513780e19de915f158ad969d5e82fc0b3f22

---
+++ protocol/eom-server-protocol.h
@@ -0,0 +1,194 @@
+#ifndef EOM_SERVER_PROTOCOL_H
+#define EOM_SERVER_PROTOCOL_H
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stddef.h>
+#include "wayland-server.h"
+
+struct wl_client;
+struct wl_resource;
+
+struct wl_eom;
+
+extern const struct wl_interface wl_eom_interface;
+
+#ifndef WL_EOM_ERROR_ENUM
+#define WL_EOM_ERROR_ENUM
+enum wl_eom_error {
+	WL_EOM_ERROR_NONE = 0,
+	WL_EOM_ERROR_NO_OUTPUT = 1,
+	WL_EOM_ERROR_NO_ATTRIBUTE = 2,
+	WL_EOM_ERROR_OUTPUT_OCCUPIED = 3,
+};
+#endif /* WL_EOM_ERROR_ENUM */
+
+#ifndef WL_EOM_TYPE_ENUM
+#define WL_EOM_TYPE_ENUM
+/**
+ * wl_eom_type - connector type of the external output
+ * @WL_EOM_TYPE_NONE: none
+ * @WL_EOM_TYPE_VGA: VGA output connector type
+ * @WL_EOM_TYPE_DIVI: VGA output connector type
+ * @WL_EOM_TYPE_DIVD: VGA output connector type
+ * @WL_EOM_TYPE_DIVA: VGA output connector type
+ * @WL_EOM_TYPE_COMPOSITE: VGA output connector type
+ * @WL_EOM_TYPE_SVIDEO: VGA output connector type
+ * @WL_EOM_TYPE_LVDS: VGA output connector type
+ * @WL_EOM_TYPE_COMPONENT: VGA output connector type
+ * @WL_EOM_TYPE_9PINDIN: VGA output connector type
+ * @WL_EOM_TYPE_DISPLAYPORT: VGA output connector type
+ * @WL_EOM_TYPE_HDMIA: VGA output connector type
+ * @WL_EOM_TYPE_HDMIB: VGA output connector type
+ * @WL_EOM_TYPE_TV: VGA output connector type
+ * @WL_EOM_TYPE_EDP: VGA output connector type
+ * @WL_EOM_TYPE_VIRTUAL: VGA output connector type
+ * @WL_EOM_TYPE_DSI: VGA output connector type
+ *
+ * ***** TODO ******
+ */
+enum wl_eom_type {
+	WL_EOM_TYPE_NONE = 0,
+	WL_EOM_TYPE_VGA = 1,
+	WL_EOM_TYPE_DIVI = 2,
+	WL_EOM_TYPE_DIVD = 3,
+	WL_EOM_TYPE_DIVA = 4,
+	WL_EOM_TYPE_COMPOSITE = 5,
+	WL_EOM_TYPE_SVIDEO = 6,
+	WL_EOM_TYPE_LVDS = 7,
+	WL_EOM_TYPE_COMPONENT = 8,
+	WL_EOM_TYPE_9PINDIN = 9,
+	WL_EOM_TYPE_DISPLAYPORT = 10,
+	WL_EOM_TYPE_HDMIA = 11,
+	WL_EOM_TYPE_HDMIB = 12,
+	WL_EOM_TYPE_TV = 13,
+	WL_EOM_TYPE_EDP = 14,
+	WL_EOM_TYPE_VIRTUAL = 15,
+	WL_EOM_TYPE_DSI = 16,
+};
+#endif /* WL_EOM_TYPE_ENUM */
+
+#ifndef WL_EOM_STATUS_ENUM
+#define WL_EOM_STATUS_ENUM
+/**
+ * wl_eom_status - connection status of the external output
+ * @WL_EOM_STATUS_NONE: none
+ * @WL_EOM_STATUS_CONNECTION: output connected
+ * @WL_EOM_STATUS_DISCONNECTION: output disconnected
+ *
+ * ***** TODO ******
+ */
+enum wl_eom_status {
+	WL_EOM_STATUS_NONE = 0,
+	WL_EOM_STATUS_CONNECTION = 1,
+	WL_EOM_STATUS_DISCONNECTION = 2,
+};
+#endif /* WL_EOM_STATUS_ENUM */
+
+#ifndef WL_EOM_MODE_ENUM
+#define WL_EOM_MODE_ENUM
+/**
+ * wl_eom_mode - mode of the external output
+ * @WL_EOM_MODE_NONE: none
+ * @WL_EOM_MODE_MIRROR: mirror mode
+ * @WL_EOM_MODE_PRESENTATION: presentation mode
+ *
+ * ***** TODO ******
+ */
+enum wl_eom_mode {
+	WL_EOM_MODE_NONE = 0,
+	WL_EOM_MODE_MIRROR = 1,
+	WL_EOM_MODE_PRESENTATION = 2,
+};
+#endif /* WL_EOM_MODE_ENUM */
+
+#ifndef WL_EOM_ATTRIBUTE_ENUM
+#define WL_EOM_ATTRIBUTE_ENUM
+/**
+ * wl_eom_attribute - attribute of the external output
+ * @WL_EOM_ATTRIBUTE_NONE: none
+ * @WL_EOM_ATTRIBUTE_NORMAL: nomal attribute
+ * @WL_EOM_ATTRIBUTE_EXCLUSIVE_SHARED: exclusive shared attribute
+ * @WL_EOM_ATTRIBUTE_EXCLUSIVE: exclusive attribute
+ *
+ * ***** TODO ******
+ */
+enum wl_eom_attribute {
+	WL_EOM_ATTRIBUTE_NONE = 0,
+	WL_EOM_ATTRIBUTE_NORMAL = 1,
+	WL_EOM_ATTRIBUTE_EXCLUSIVE_SHARED = 2,
+	WL_EOM_ATTRIBUTE_EXCLUSIVE = 3,
+};
+#endif /* WL_EOM_ATTRIBUTE_ENUM */
+
+#ifndef WL_EOM_ATTRIBUTE_STATE_ENUM
+#define WL_EOM_ATTRIBUTE_STATE_ENUM
+/**
+ * wl_eom_attribute_state - state of the external output attribute
+ * @WL_EOM_ATTRIBUTE_STATE_NONE: none
+ * @WL_EOM_ATTRIBUTE_STATE_ACTIVE: attribute is active on the output
+ * @WL_EOM_ATTRIBUTE_STATE_INACTIVE: attribute is inactive on the output
+ * @WL_EOM_ATTRIBUTE_STATE_LOST: the connection of output is lost
+ *
+ * ***** TODO ******
+ */
+enum wl_eom_attribute_state {
+	WL_EOM_ATTRIBUTE_STATE_NONE = 0,
+	WL_EOM_ATTRIBUTE_STATE_ACTIVE = 1,
+	WL_EOM_ATTRIBUTE_STATE_INACTIVE = 2,
+	WL_EOM_ATTRIBUTE_STATE_LOST = 3,
+};
+#endif /* WL_EOM_ATTRIBUTE_STATE_ENUM */
+
+/**
+ * wl_eom - an interface to get the information of the external outputs
+ * @set_attribute: (none)
+ *
+ * ***** TODO ******
+ */
+struct wl_eom_interface {
+	/**
+	 * set_attribute - (none)
+	 * @output: (none)
+	 * @attribute: (none)
+	 */
+	void (*set_attribute)(struct wl_client *client,
+			      struct wl_resource *resource,
+			      struct wl_resource *output,
+			      uint32_t attribute);
+};
+
+#define WL_EOM_OUTPUT_TYPE	0
+#define WL_EOM_OUTPUT_MODE	1
+#define WL_EOM_OUTPUT_ATTRIBUTE	2
+
+#define WL_EOM_OUTPUT_TYPE_SINCE_VERSION	1
+#define WL_EOM_OUTPUT_MODE_SINCE_VERSION	1
+#define WL_EOM_OUTPUT_ATTRIBUTE_SINCE_VERSION	1
+
+static inline void
+wl_eom_send_output_type(struct wl_resource *resource_, struct wl_resource *output, uint32_t type, uint32_t status, uint32_t error)
+{
+	wl_resource_post_event(resource_, WL_EOM_OUTPUT_TYPE, output, type, status, error);
+}
+
+static inline void
+wl_eom_send_output_mode(struct wl_resource *resource_, struct wl_resource *output, uint32_t mode, uint32_t error)
+{
+	wl_resource_post_event(resource_, WL_EOM_OUTPUT_MODE, output, mode, error);
+}
+
+static inline void
+wl_eom_send_output_attribute(struct wl_resource *resource_, struct wl_resource *output, uint32_t attribute, uint32_t attribute_state, uint32_t error)
+{
+	wl_resource_post_event(resource_, WL_EOM_OUTPUT_ATTRIBUTE, output, attribute, attribute_state, error);
+}
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif
